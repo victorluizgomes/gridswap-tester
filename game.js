130eef159dc1469b9089a83b960714811e0578ce
Victor Gomes <gomesvictorluiz@gmail.com>
2022-03-04 12:29:12 -0500

    add initial files

---
+++ game.js
@@ -0,0 +1,346 @@
+class Graph {
+  constructor(randomSpaces) {
+    this.adjacencyList = {};
+    if (randomSpaces) {
+        this.emptySpaces = randomSpaces;
+    } else {
+        this.emptySpaces = [14, 22, 55, 58];
+    }
+    this.setEmptySpaces()
+  }
+  setEmptySpaces() {
+      let blankTile = document.getElementById(this.emptySpaces[0]);
+      blankTile.classList.add("blocked-tile");
+      blankTile = document.getElementById(this.emptySpaces[1]);
+      blankTile.classList.add("blocked-tile");
+      blankTile = document.getElementById(this.emptySpaces[2]);
+      blankTile.classList.add("blocked-tile");
+      blankTile = document.getElementById(this.emptySpaces[3]);
+      blankTile.classList.add("blocked-tile");
+  }
+
+//   SOLVING ATTEMPT BELLOW
+//   addVertex(vertex) {
+//     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+//   }
+//   addEdge(v1, v2) {
+//     if (this.emptySpaces.includes(v2)) return;
+//     if (!this.adjacencyList[v1].includes(v2)) {
+//       this.adjacencyList[v1].push(v2);
+//     }
+//     if (!this.adjacencyList[v2].includes(v1)) {
+//       this.adjacencyList[v2].push(v1);
+//     }
+//   }
+//   removeEdge(vertex1, vertex2) {
+//     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+//       (v) => v !== vertex2
+//     );
+//     this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+//       (v) => v !== vertex1
+//     );
+//   }
+//   removeVertex(vertex) {
+//     while (this.adjacencyList[vertex].length) {
+//       const adjacentVertex = this.adjacencyList[vertex].pop();
+//       this.removeEdge(vertex, adjacentVertex);
+//     }
+//     delete this.adjacencyList[vertex];
+//   }
+//   depthFirstRecursive(start) {
+//     const result = [];
+//     const visited = {};
+//     const adjacencyList = this.adjacencyList;
+
+//     (function dfs(vertex) {
+//       if (!vertex) return null;
+//       visited[vertex] = true;
+//       result.push(vertex);
+//       adjacencyList[vertex].forEach((neighbor) => {
+//         if (!visited[neighbor]) {
+//           return dfs(neighbor);
+//         }
+//       });
+//     })(start);
+
+//     return result;
+//   }
+//   depthFirstIterative(start) {
+//     const stack = [start];
+//     const result = [];
+//     const visited = {};
+//     let currentVertex;
+
+//     visited[start] = true;
+//     while (stack.length) {
+//       currentVertex = stack.pop();
+//       result.push(currentVertex);
+
+//       this.adjacencyList[currentVertex].forEach((neighbor) => {
+//         if (!visited[neighbor]) {
+//           visited[neighbor] = true;
+//           stack.push(neighbor);
+//         }
+//       });
+//     }
+//     return result;
+//   }
+//   breadthFirst(start) {
+//     const queue = [start];
+//     const result = [];
+//     const visited = {};
+//     let currentVertex;
+//     visited[start] = true;
+
+//     while (queue.length) {
+//       currentVertex = queue.shift();
+//       result.push(currentVertex);
+
+//       this.adjacencyList[currentVertex].forEach((neighbor) => {
+//         if (!visited[neighbor]) {
+//           visited[neighbor] = true;
+//           queue.push(neighbor);
+//         }
+//       });
+//     }
+//     return result;
+//   }
+}
+
+// let g = new Graph();
+// for (let i = 0; i < 100; i++) {
+//   let vertex = i.toString();
+//   g.addVertex(vertex);
+// }
+
+// for (let i = 0; i < 100; i++) {
+//   let vertex = i.toString();
+//   if (g.emptySpaces.includes(vertex)) {
+//     continue;
+//   }
+//   let leftEdge = false;
+//   let topEdge = false;
+//   let rightEdge = false;
+//   let bottomEdge = false;
+//   if (i < 10) topEdge = true;
+//   if (i % 10 === 0) leftEdge = true;
+//   if (i >= 90) bottomEdge = true;
+//   if ((i - 9) % 10 === 0) rightEdge = true;
+//   let topVertex = (i - 10).toString();
+//   let leftVertex = (i - 1).toString();
+//   let rightVertex = (i + 1).toString();
+//   let bottomVertex = (i + 10).toString();
+//   switch (true) {
+//     case leftEdge && bottomEdge:
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, rightVertex);
+//       break;
+//     case leftEdge && topEdge:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, rightVertex);
+//       break;
+//     case rightEdge && bottomEdge:
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//     case rightEdge && topEdge:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//     case rightEdge:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//     case leftEdge:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, rightVertex);
+//       break;
+//     case topEdge:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, rightVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//     case bottomEdge:
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, rightVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//     default:
+//       g.addEdge(vertex, bottomVertex);
+//       g.addEdge(vertex, rightVertex);
+//       g.addEdge(vertex, topVertex);
+//       g.addEdge(vertex, leftVertex);
+//       break;
+//   }
+// }
+
+// USER MOVEMENT GAME
+let currentPosition = 0;
+let actionOrders = [];
+
+let random = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)];
+let g = new Graph(random);
+
+function randomizeBoard() {
+    random = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)];
+    g = new Graph(random);
+}
+
+function clearBoardRandomize() {
+    this.clearBoard(false);
+    this.randomizeBoard();
+}
+
+function clearBoard(addTiles) {
+    var odds = true;
+    var one = document.getElementById("0");
+    one.className = "tile player";
+    this.removeAllChildNodes(one);
+    one.innerHTML = "0";
+    var curr = null;
+    for(var i = 1; i <= 99; i++) {
+        if (i % 10 === 0) {
+            odds = !odds;
+        }
+        if (odds) {
+            if (i % 2 !== 0) {
+                curr = document.getElementById(i.toString());
+                curr.className = "";
+                curr.classList.add("tile-b");
+            } else {
+                curr = document.getElementById(i.toString());
+                curr.className = "";
+                curr.classList.add("tile");
+            }
+        } else {
+            if (i % 2 !== 0) {
+                curr = document.getElementById(i.toString());
+                curr.className = "";
+                curr.classList.add("tile");
+            } else {
+                curr = document.getElementById(i.toString());
+                curr.className = "";
+                curr.classList.add("tile-b");
+            }
+        }
+        this.removeAllChildNodes(curr);
+        curr.innerHTML = i.toString();
+    }
+    currentPosition = 0;
+    actionOrders = [];
+
+    if (addTiles)
+        g = new Graph(random);
+
+}
+
+document.addEventListener('keydown', function(event) {
+    console.log('Current Position', currentPosition);
+    if(event.keyCode == 37) {
+        console.log('Moved Left');
+        actionOrders.push('left');
+        curr = document.getElementById(currentPosition.toString());
+        currentPosition -= 1;
+        curr.classList.add("visited");
+
+        var bar = document.createElement('div');
+        if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "down") {
+            bar.classList.add("down-left");
+        } else if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "up") {
+            bar.classList.add("up-left");
+        } else {
+            bar.classList.add("hl");
+        }
+        curr.appendChild(bar);
+
+        curr = document.getElementById(currentPosition.toString());
+        curr.classList.add("player");
+    }
+    else if(event.keyCode == 39) {
+        console.log('Moved Right');
+        actionOrders.push('right');
+        curr = document.getElementById(currentPosition.toString());
+        currentPosition += 1;
+        curr.classList.add("visited");
+
+        var bar = document.createElement('div');
+        if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "down") {
+            bar.classList.add("down-right");
+        } else if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "up") {
+            bar.classList.add("up-right");
+        } else {
+            bar.classList.add("hl");
+        }
+        curr.appendChild(bar);
+        curr = document.getElementById(currentPosition.toString());
+        curr.classList.add("player");
+    }
+    else if(event.keyCode == 38) {
+        console.log('Moved Up');
+        actionOrders.push('up');
+        curr = document.getElementById(currentPosition.toString());
+        currentPosition -= 10;
+        curr.classList.add("visited");
+
+        var bar = document.createElement('div');
+        if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "right") {
+            bar.classList.add("down-left");
+        } else if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "left") {
+            bar.classList.add("down-right");
+        } else {
+            bar.classList.add("vl");
+        }
+        curr.appendChild(bar);
+        curr = document.getElementById(currentPosition.toString());
+        curr.classList.add("player");
+    }
+    else if(event.keyCode == 40) {
+        console.log('Moved Down');
+        actionOrders.push('down');
+        curr = document.getElementById(currentPosition.toString());
+        currentPosition += 10;
+        curr.classList.add("visited");
+
+        var bar = document.createElement('div');
+        if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "right") {
+            bar.classList.add("up-left");
+        } else if (actionOrders.length > 1 && actionOrders[actionOrders.length - 2] === "left") {
+            bar.classList.add("up-right");
+        } else {
+            bar.classList.add("vl");
+        }
+        curr.appendChild(bar);
+        curr = document.getElementById(currentPosition.toString());
+        curr.classList.add("player");
+    }
+    //console.log(actionOrders);
+});
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//console.log(g.depthFirstRecursive("0"));
+
+// 0  1  2  3  4  5  6  7  8  9
+// 10 11 12 13 14 15 16 17 18 19
+// 20 21 22 23 24 25 26 27 28 29
+// 30 31 32 33 34 35 36 37 38 39
+// 40 41 42 43 44 45 46 47 48 49
+// 50 51 52 53 54 55 56 57 58 59
+// 60 61 62 63 64 65 66 67 68 69
+// 70 71 72 73 74 75 76 77 78 79
+// 80 81 82 83 84 85 86 87 88 89
+// 90 91 92 93 94 95 96 97 98 99
+
+// LIST OF GAMES
+// 16, 73, 78, 97 empty spaces
+// 11, 26, 50, 79
+// 21, 25, 50, 93
+// 4, 46, 64, 81 (impossible)
+// 14, 22, 36, 58 (impossible)
+// 14, 22, 55, 58
